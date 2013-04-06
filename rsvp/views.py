5e6836f1a6d0682cc6de5ab74a55a523fd1f4655
bahoo <jon.c.culver@gmail.com>
2013-04-06 14:18:56 -0700

    Holy hell RSVP's are almost done.

--- rsvp/views.py
+++ rsvp/views.py
@@ -14,5 +14,13 @@ def save_the_date(request):
 
 def rsvp(request, key):
     rsvp = Rsvp.objects.get(key=key)
-    rsvp_form = RsvpForm()
-    return render(request, 'rsvp.html', {'rsvp': rsvp, 'rsvp_form': rsvp_form})
+    rsvp_form = RsvpForm(request.POST or None, instance=rsvp)
+    submitted = False
+    if request.POST:
+        submitted = True
+        if rsvp_form.is_valid():
+            rsvp_form.save()
+        else:
+            print rsvp_form.errors
+    already_rsvped = (rsvp.is_attending is not None)
+    return render(request, 'rsvp.html', {'rsvp': rsvp, 'rsvp_form': rsvp_form, 'submitted': submitted, 'already_rsvped': already_rsvped})
